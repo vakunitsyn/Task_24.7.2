2f2215734afd161d09a5cbf3ae5837e588cbbf61
vakunitsyn <vakunitsyn@yandex.ru>
2023-05-12 00:43:00 +0300

    settings

---
+++ settings.py
@@ -0,0 +1,6 @@
+valid_email = "vakunitsyn@yandex.ru"
+valid_password = "15az4sdm"
+empty_email = ""
+empty_password = ""
+empty_auth_key = {"key": ""}
+incorrect_auth_key = {"key": "000000000000000000000000000000000000000000000"}
